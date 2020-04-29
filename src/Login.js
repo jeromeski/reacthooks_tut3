e960f8b73df733ab7918a57d23388463b3abf4d4
Jeromeski <jeromegee@gmail.com>
2020-04-29 08:23:31 +0800

    Implement a login form w/ multiple state values

---
+++ src/Login.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null)
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    const userData = {
+      username,
+      password
+    }
+    setUser(JSON.stringify(userData))
+  }
+
+  return (
+    <div
+      stle={{
+        textAlign: 'center'
+      }}
+    >
+      <h2>Login</h2>
+        <form 
+          onSubmit={handleSubmit}
+        >
+          <label>username</label>
+          <input 
+            type="text" 
+            placeholder="Username"
+            onChange={(evt) => setUsername(evt.target.value)}
+            value={username}
+          />
+          <label>password</label>
+          <input 
+            type="password" 
+            placeholder="Password"
+            onChange={(evt) => setPassword(evt.target.value)}
+            value={password}
+          />
+          <button>submit</button>
+        </form>
+        { user ? user : null}
+    </div>
+  );
+};
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // import App from './AppClass';
-import App from './AppFunction';
+// import App from './AppFunction';
+// import App from './Register';
+import App from './Login';
+
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
